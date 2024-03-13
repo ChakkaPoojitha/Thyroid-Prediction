6913cf065915eaeb799f96d05ea45aceef65d30d
ChakkaPoojitha <110971423+ChakkaPoojitha@users.noreply.github.com>
2024-03-13 15:34:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def predict():
 
     return render_template('result.html',prediction=my_prediction)
 if __name__=='__main__':
-    app.run(debug=True)
+    classifier=joblib.load('final.pkl')
+    app.run(host='0.0.0.0',port=5000,debug=True)
